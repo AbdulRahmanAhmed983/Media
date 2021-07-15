ac94e3e3f3c1e479731b1a2016aaaf4e3730a0c5
Abdalrhman Hussin <abdalrhmanhussin44@gmail.com>
2021-07-15 21:50:03 +0200

    a

--- actions/Controllers/HomeController.php
+++ actions/Controllers/HomeController.php
@@ -3,9 +3,10 @@
 class HomeController {
     public static function index() {
         if(session_status() == PHP_SESSION_NONE) {
-            template::view('Dashboard');
-        } else {
             header('location:/login');
+
+        } else {
+            template::view('Dashboard');
         }
     }
 }
